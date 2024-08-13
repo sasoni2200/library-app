638b5511bcd531f4ae67999612d20c1c0aa452d6
Sagar Soni <sagar.soni.may1998@gmail.com>
2024-08-13 13:48:01 +0530

    updatnonenv

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyAI-7IUqRvvujW1iTBMMSmhUs512yJOrQ4",
+  authDomain: "app-efd1e.firebaseapp.com",
+  projectId: "app-efd1e",
+  storageBucket: "app-efd1e.appspot.com",
+  messagingSenderId: "421651516908",
+  appId: "1:421651516908:web:23dd5b38a1da02028844c9",
+  databaseURL: "https://app-efd1e-default-rtdb.firebaseio.com/",
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
